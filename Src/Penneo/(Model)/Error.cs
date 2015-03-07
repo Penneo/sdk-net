716163e7c8539f0e67d4ef13b9199015f17153ef
Christian Andersen <chrsadk@gmail.com>
2015-03-07 06:59:24 +0100

    Improved error handling for queries

---
+++ Src/Penneo/(Model)/Error.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace Penneo
+{
+    public class Error
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool ServerNotFound { get { return (int) StatusCode == 0 || StatusCode == HttpStatusCode.NotFound; } }
+        public string ErrorMessage { get; set; }
+    }
+}
