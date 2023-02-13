1bd2076c04887fdd10001e5098a427c8ebdaf56e
Jørgen Jensen <53434466+HeadCookie@users.noreply.github.com>
2023-02-13 12:41:24 +0100

    Update newtonsoft.json depedency

--- Src/Penneo/Info.cs
+++ Src/Penneo/Info.cs
@@ -5,6 +5,6 @@ namespace Penneo
         /// <summary>
         /// The version of the SDK. This should be updated on each release.
         /// </summary>
-        public const string Version = "5.3.0";
+        public const string Version = "5.3.1";
     }
 }
