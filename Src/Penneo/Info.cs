7c0cd8ba532fd37340b5b6a13b41abb320a4a93b
Jørgen <53434466+HeadCookie@users.noreply.github.com>
2025-01-09 11:08:08 +0100

    docs: add descriptions for SocialSecurityNumber and SsnType on Signer (#181)

--- Src/Penneo/Info.cs
+++ Src/Penneo/Info.cs
@@ -5,6 +5,6 @@ namespace Penneo
         /// <summary>
         /// The version of the SDK. This should be updated on each release.
         /// </summary>
-        public const string Version = "6.1.2";
+        public const string Version = "7.0.4";
     }
 }
